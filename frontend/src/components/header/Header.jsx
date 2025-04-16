5f0579dc28335d56a1d190c803d7a9624ca9c231
Shalap <shalappandotra21@email.com>
2025-04-16 14:17:42 +0530

    second commit

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,11 +1,103 @@
+import logo from "../../assets/images/logo.png";
+import { NavLink, Link } from "react-router-dom";
+import userImg from "../../assets/images/avatar-icon.png";
+import { useEffect, useRef } from "react";
+import { BiMenu } from "react-icons/bi";
 
-
-import React from 'react'
+const navLinks = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/doctors",
+    display: "Find a Doctor",
+  },
+  {
+    path: "/services",
+    display: "Services",
+  },
+  {
+    path: "/contact",
+    display: "Contact",
+  },
+];
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const headerRef = useRef(null);
+  const menuRef = useRef(null);
 
-export default Header
+  const handleStickyHeader = () => {
+    window.addEventListener(scroll, () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add("sticky__header");
+      } else {
+        headerRef.current.classList.remove("sticky__header");
+      }
+    });
+  };
+
+  useEffect(() => {
+    handleStickyHeader();
+
+    return () => window.removeEventListener("scroll", handleStickyHeader);
+  });
+
+  const toggleMenu = () => menuRef.current.classList.toggle("show__menu");
+
+  return (
+    <header className="header flex items-center" ref={headerRef}>
+      <div className="container">
+        <div className="flex items-center justify-between">
+          <div>
+            <img src={logo} alt="" />
+          </div>
+
+          <div className="navigation" ref={menuRef} onClick={toggleMenu}>
+            <ul className="menu flex items-center gap-[2.7rem]">
+              {navLinks.map((link, index) => (
+                <li key={index}>
+                  <NavLink
+                    to={link.path}
+                    className={(navClass) =>
+                      navClass.isActive
+                        ? "text-primaryColour text-[16px] leading-7 font-[600]"
+                        : "text-textColour text-[16px] leading-7 font-[500] hover:text-primaryColour"
+                    }
+                  >
+                    {link.display}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="flex items-center gap-4">
+            <div className="hidden">
+              <Link to="/">
+                <figure className="w-[35px] h-[35px] rounded-full cursor-pointer py-2">
+                  <img src={userImg} alt="" />
+                </figure>
+              </Link>
+            </div>
+
+            <Link to="/login">
+              <button className="bg-primaryColour my-2 py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[7.5px]">
+                Login
+              </button>
+            </Link>
+
+            <span className="md:hidden" onClick={toggleMenu}>
+              <BiMenu className="w-6 h-6 cursor-pointer"></BiMenu>
+            </span>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primaryColour: "#0067FF",
+        yellowColour: "#FEB60D",
+        purpleColour: "#9771FF",
+        irisBlueColour: "#01B5C5",
+        headingColour: "#181A1E",
+        textColour: "#4E545F"
+      },
+      boxShadow: {
+        panelShadow: "rgba(17, 12, 46, 0.15) 0px 48px 100px 0px"
+      }
+    },
+  },
+  plugins: [],
+}
